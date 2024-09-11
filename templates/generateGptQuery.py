a936cfbfe67b42d4029adb43c911971d3436117c
Leon Hornich <leonhornich@alocalhotspot.speedport_w_921v_1_48_000>
2024-09-11 15:51:17 +0200

    adjusted some formating

--- templates/generateGptQuery.py
+++ templates/generateGptQuery.py
@@ -10,32 +10,34 @@ mode = "${mode}"
 question = "${question}"
 
 # Open data file
-with open(data_path, 'r') as file:
+with open(data_path, "r") as file:
     # Read the header row and split into column names
-    header = file.readline().strip().split('\t')
+    header = file.readline().strip().split("\t")
 
     # Check if specified column exists
     if target_column not in header:
-        print(f"Error: The specified column '{target_column}' was not found in the data file!")
-    
+        print(
+            f"Error: The specified column '{target_column}' was not found in the data file!"
+        )
+
     # Find the target column index
     target_index = header.index(target_column)
 
     # Initiate list to store gene id's with corresponding data values
     data = []
-    
+
     # Prepare data file's rows
     for line in file:
-        row = line.strip().split('\t')
+        row = line.strip().split("\t")
         gene_id = row[0]
         value = float(row[target_index])
         data.append((gene_id, value))
 
     # Sort the data based on provided mode
     if mode == "low":
-        sorted_data = sorted(data, key=lambda x: x[1])  
+        sorted_data = sorted(data, key=lambda x: x[1])
     elif mode == "high":
-        sorted_data = sorted(data, key=lambda x: x[1], reverse=True)  
+        sorted_data = sorted(data, key=lambda x: x[1], reverse=True)
     else:
         print("Error: Please provide either 'low' or 'high' as mode.")
 
@@ -43,7 +45,7 @@ with open(data_path, 'r') as file:
     top_gene_ids = [gene_id for gene_id, value in sorted_data[:num_genes]]
 
     # Write everything into a output file
-    with open("gpt_${source}_query.txt", 'w') as query_file:
-        query_file.write(question + '''\n''')
+    with open("gpt_${source}_query.txt", "w") as query_file:
+        query_file.write(question + """\n""")
         for gene_id in top_gene_ids:
-            query_file.write(gene_id + '''\n''')
+            query_file.write(gene_id + """\n""")
